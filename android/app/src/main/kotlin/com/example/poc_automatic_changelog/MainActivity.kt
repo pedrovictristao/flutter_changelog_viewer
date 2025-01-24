c6ab364df95b68e9b798d33150af465c724a5fa4
Pedro <pedrotristao@chiptronic.com.br>
2025-01-24 16:33:54 -0300

    feat: - Criação do projeto. - Adição da dependência "changelog_cli". - Criação do arquivo "changelog.yaml"

---
+++ android/app/src/main/kotlin/com/example/poc_automatic_changelog/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.poc_automatic_changelog
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'POC Changelog automático',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text('Homepage'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'Pressione o botão abaixo para gerar o histórico de versões!',
+            ),
+            ElevatedButton(onPressed: () {}, child: const Text('Histórico de versões'))
+          ],
+        ),
+      ),
+    );
+  }
+}
